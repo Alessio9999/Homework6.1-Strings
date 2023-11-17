fb4da5190c1a0da5e414499b6b9c27e977d3e700
LeXX <lexx8665@yandex.ru>
2023-11-17 05:47:53 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        Task1();
+        Task2();
+        Task3();
+    }
+
+    private static void Task1() {
+        System.out.println("\nЗадача 1:");
+
+    }
+
+    private static void Task2() {
+        System.out.println("\nЗадача 2:");
+
+    }
+
+    private static void Task3() {
+        System.out.println("\nЗадача 3:");
+
+    }
+
+}
