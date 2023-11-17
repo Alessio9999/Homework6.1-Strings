1361c7872555c269d9c66c5458c851137698a0ed
LeXX <lexx8665@yandex.ru>
2023-11-17 05:55:26 +0500

    Выполнил ДЗ 6.1 (Strings)

--- src/Main.java
+++ src/Main.java
@@ -7,17 +7,25 @@ public class Main {
 
     private static void Task1() {
         System.out.println("\nЗадача 1:");
-
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("Ф. И. О. сотрудника — " + fullName);
     }
 
     private static void Task2() {
         System.out.println("\nЗадача 2:");
-
+        String fullName = "Ivanov Ivan Ivanovich";
+        String fullNameUpperCased = fullName.toUpperCase();
+        System.out.println(fullNameUpperCased);
     }
 
     private static void Task3() {
         System.out.println("\nЗадача 3:");
-
+        String fullName = "Иванов Семён Семёнович";
+        String fullNameChanged = fullName.replace('ё','е');
+        System.out.println("Данные Ф. И. О. сотрудника — " + fullNameChanged);
     }
 
 }
